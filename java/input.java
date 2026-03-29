e6a67ba7383e43b7e754499e220bf33ec80fafea
praveen_shikhare <praveenshikhare4@gmail.com>
2026-03-29 14:03:29 +0530

    Add files via upload

---
+++ java/input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int r=sc.nextInt();
+        float radius = 3.14f* r * r;
+        System.out.println(radius);
+       
+        
+    }   
+}
