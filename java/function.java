8af8e466d4fcfb9fefbb1d129a567bc9992af883
praveen_shikhare <praveenshikhare4@gmail.com>
2026-04-04 14:52:04 +0530

    Add files via upload

--- java/function.java
+++ java/function.java
@@ -79,15 +79,93 @@ public class function {
     // }
 
     // table of a number
-    public static void printTable(int n){
-        for(int i=1; i<=10; i++){
-            System.out.println(n*i);   
+    // public static void printTable(int n){
+    //     for(int i=1; i<=10; i++){
+    //         System.out.println(n*i);   
+    //     }
+    // } 
+    // public static void main(String[] args) {
+    //     Scanner Sc=new Scanner(System.in);
+    //     int n=Sc.nextInt();
+    //     printTable(n);
+    // }
+    // public static float printAverage(int a, int b, int c){
+    //     float average=(a+b+c)/3;
+    //     return average;
+    // }
+    // public static void main(String[] args) {
+    //     Scanner Sc=new Scanner(System.in);
+    //     int a=Sc.nextInt();
+    //      Scanner sc=new Scanner(System.in);
+    //     int b=sc.nextInt();
+    //      Scanner SC=new Scanner(System.in);
+    //     int c=SC.nextInt();
+    //     float average=printAverage(a,b,c);
+    //     System.out.println("average of 3 number is :"+average);
+    // }
+
+    // public static void printSumOfOddnumbers(int n){
+    //     int sum=0;
+        
+    //     for(int i=1; i<=n; i++){
+    //         if(i%2 !=0){
+    //             sum+=i;
+
+    //         }
+            
+    //     }
+    //     System.out.println("sum of odd numbers:"+sum);
+            
+          
+    // }
+    // public static void main(String[] args) {
+    //     Scanner Sc=new Scanner(System.in);
+    //     int n=Sc.nextInt();
+    //     printSumOfOddnumbers(n);
+    // }
+    // public static void printGreatestOfTwo(int a, int b){
+    //     if(a>b){
+    //         System.out.println(a+"is greater than"+b);
+    //         return;
+    //     }else if(b>a){
+    //         System.out.println(b+"is greater than"+a);
+    //         return;
+    //     }else if(a==b){
+    //         System.out.println(a+"is equal to"+ b);
+    //     }else{
+    //         System.out.println("invalid choice");
+    //     }
+
+    // }
+    // public static void main(String[] args) {
+    //     Scanner Sc=new Scanner(System.in);
+    //     int a=Sc.nextInt();
+    //     Scanner sc=new Scanner(System.in);
+    //     int b=Sc.nextInt();
+    //     printGreatestOfTwo(a, b);
+    // }
+    // public static void printCircumferenceOfCircle(int r){
+    //     double circumference=2*3.14*r;
+    //     System.out.println("circumference of circle: "+circumference);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner Sc=new Scanner(System.in);
+    //     int r=Sc.nextInt();
+    //     printCircumferenceOfCircle(r);
+
+    // }
+    public static void printAgeForVote(int age){
+        if(age>=18){
+            System.out.println("person eligible for vote");
+        }else{
+            System.out.println("person is not eligible for vote");
         }
-    } 
+    }
     public static void main(String[] args) {
         Scanner Sc=new Scanner(System.in);
-        int n=Sc.nextInt();
-        printTable(n);
+        int age=Sc.nextInt();
+        printAgeForVote(age);
     }
-    
+
+
 }
